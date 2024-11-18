5bb3ca18fbd827c3e58cd02cb2eed85b4a0d81fb
Allec C. L. <50810927+allec9@users.noreply.github.com>
2024-11-18 20:50:31 -0300

    Add files via upload

---
+++ uga.js
@@ -0,0 +1,18 @@
+var numero1 = document.querySelector("#numero1")
+var numero2 = document.querySelector("#numero2")
+var btnSoma = document.querySelector("#btnSoma")
+var btnDivisao = document.querySelector("#btnDivisao")
+var btnMiltiplicacao = document.querySelector("#btnSubtracao")
+var btnSubtracao = document.querySelector("#btnSubtracao")
+
+btnSoma.addEventListener("click",somar)
+
+btnDivisao.addEventListener("click",dividir)
+
+btnMiltiplicacao.addEventListener("click",multiplicar)
+
+btnSubtracao.addEventListener("click",subtrair)
+
+function somar(){
+    res.innerText = Number(numero1.value) + Number(numero2.value)
+}
