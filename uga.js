5054362be93bded04c64632ed1ba11d4470cd33a
Allec C. L. <50810927+allec9@users.noreply.github.com>
2024-11-21 20:34:11 -0300

    Add files via upload

--- uga.js
+++ uga.js
@@ -2,10 +2,13 @@ var numero1 = document.querySelector("#numero1")
 var numero2 = document.querySelector("#numero2")
 var btnSoma = document.querySelector("#btnSoma")
 var btnDivisao = document.querySelector("#btnDivisao")
-var btnMiltiplicacao = document.querySelector("#btnSubtracao")
+var btnMiltiplicacao = document.querySelector("#btnMultiplicacao")
 var btnSubtracao = document.querySelector("#btnSubtracao")
 
 btnSoma.addEventListener("click",somar)
+btnDivisao.addEventListener("click", dividir)
+btnSubtracao.addEventListener("click", subtrair)
+btnMiltiplicacao.addEventListener("click", multiplicar)
 
 btnDivisao.addEventListener("click",dividir)
 
@@ -15,4 +18,14 @@ btnSubtracao.addEventListener("click",subtrair)
 
 function somar(){
     res.innerText = Number(numero1.value) + Number(numero2.value)
+}
+function dividir() {
+    res.innerText = Number(numero1.value) / Number(numero2.value)
+
+}
+function subtrair() {
+    res.innerText = Number(numero1.value) - Number(numero2.value)
+}
+function multiplicar() {
+    res.innerText = Number(numero1.value) * Number(numero2.value)
 }
